cc1fcda28488c481564ee1ecdb2e0a0fe3fb6779
tatarnirartha <tatarnirartha@gmail.com>
2022-10-03 18:12:04 +0800

    Fix get data

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/getAllData', 'Mocking\Controllers\Controller@getData');
+Route::get('/getAllData', 'Mocking\Controllers\ExampleController@getData');
 Route::post('/postData', 'Mocking\Controllers\ExampleController@postData');
 Route::put('/putData', 'Mocking\Controllers\ExampleController@putData');
 Route::patch('/patchData', 'Mocking\Controllers\ExampleController@patchData');
